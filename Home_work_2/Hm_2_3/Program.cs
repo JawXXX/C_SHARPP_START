f8cbe62c6026888cd1cb27f2556ed7f5aa35e8d2
slava <jaws_x@mail.ru>
2024-02-11 20:45:03 +0500

    Home_work_2_3_Lesson_2

---
+++ Home_work_2/Hm_2_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int maxDigit = 0;
+
+        if (number >= 10 && number <= 99)
+        {
+            while (number > 0)
+            {
+                int digit = number % 10;
+                if (digit > maxDigit)
+                {
+                    maxDigit = digit;
+                }
+                number /= 10;
+            }
+
+            Console.WriteLine("Наибольшая цифра числа: " + maxDigit);
+        }
+        else
+        {
+            Console.WriteLine("Число не входит в диапазон [10, 99]");
+        }
+    }
+}
