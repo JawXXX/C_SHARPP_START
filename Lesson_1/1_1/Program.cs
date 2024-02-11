f049964b7da661c5d022890502948e183e717390
slava <jaws_x@mail.ru>
2024-02-11 16:02:45 +0500

    Lesson 1 Task_1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// 1_Задачв
+
+Console.WriteLine("Enter a number 1: ");
+int b1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter a number 2: ");
+int b2 = int.Parse(Console.ReadLine()!);
+
+if (b2 * b2 == b1) 
+Console.WriteLine("Yes");
+else
+Console.WriteLine("No");
