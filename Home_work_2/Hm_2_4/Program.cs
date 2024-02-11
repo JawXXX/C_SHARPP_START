5e9e42549eb282a95fdc27c6b1b7e562af8a2602
slava <jaws_x@mail.ru>
2024-02-11 20:45:33 +0500

    Home_work_2_4_Lesson_2

---
+++ Home_work_2/Hm_2_4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        string digits = "";
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+            digits = digit + "," + digits;
+            number /= 10;
+        }
+
+        Console.WriteLine("Цифры числа через запятую: " + digits.TrimEnd(','));
+    }
+}
