0afabb0e67e82b7547af65872174c109f5928e2a
slava <jaws_x@mail.ru>
2024-02-11 20:42:33 +0500

    Home_work_2_Lesson

---
+++ Home_work_2/Hm_2_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 7 == 0 && number % 23 == 0)
+        {
+            Console.WriteLine("Число кратно 7 и 23");
+        }
+        else
+        {
+            Console.WriteLine("Число не кратно 7 и 23");
+        }
+    }
+}
