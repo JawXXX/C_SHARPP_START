8b9765493f549e7eba0ba468c3a2d6dc17f5d21c
slava <jaws_x@mail.ru>
2024-02-17 02:46:16 +0500

    Home work _4_1.2.3

---
+++ Home_work_4/hm_4_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Введите целое число или 'q' для выхода:");
+            string input = Console.ReadLine();
+
+            if (input == "q")
+            {
+                break;
+            }
+
+            int number;
+            if (int.TryParse(input, out number))
+            {
+                if (IsSumOfDigitsEven(number))
+                {
+                    Console.WriteLine("Сумма цифр числа чётная. Программа завершена.");
+                    break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод. Повторите попытку.");
+            }
+        }
+    }
+
+    static bool IsSumOfDigitsEven(int number)
+    {
+        int sum = 0;
+        number = Math.Abs(number);
+
+        while (number > 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        return sum % 2 == 0;
+    }
+}
