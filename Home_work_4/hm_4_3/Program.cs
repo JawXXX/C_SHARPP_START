b559aee0ab0320f4c0a68aea1194f8478387e1d8
slava <jaws_x@mail.ru>
2024-02-17 02:47:58 +0500

    Home_work_4_3

---
+++ Home_work_4/hm_4_3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("Исходный массив:");
+        PrintArray(array);
+
+        ReverseArray(array);
+
+        Console.WriteLine("Перевернутый массив:");
+        PrintArray(array);
+    }
+
+    static void ReverseArray(int[] array)
+    {
+        int start = 0;
+        int end = array.Length - 1;
+
+        while (start < end)
+        {
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (int element in array)
+        {
+            Console.Write(element + " ");
+        }
+        Console.WriteLine();
+    }
+}
