bba3fd3e0af4d3bfc84be7172a634b8f8e8c7c76
slava <jaws_x@mail.ru>
2024-02-17 02:47:29 +0500

    Home_work_4_2

---
+++ Home_work_4/hm_4_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Создаем генератор случайных чисел
+        Random random = new Random();
+
+        // Создаем массив из 10 случайных трехзначных чисел
+        int[] numbers = new int[10];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(100, 1000);
+        }
+
+        // Подсчитываем количество четных чисел
+        int count = 0;
+        foreach (int number in numbers)
+        {
+            if (number % 2 == 0)
+            {
+                count++;
+            }
+        }
+
+        // Выводим результат
+        Console.WriteLine("Массив чисел:");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine("Количество четных чисел: " + count);
+    }
+}
