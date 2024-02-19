f2f6a3435ff986a9782da52badf9eef1c074e836
slava <jaws_x@mail.ru>
2024-02-20 02:39:57 +0500

    Home_work_7_2

---
+++ Home_work_7/Hm_7_2/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static int A(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0)
+        {
+            return A(m - 1, 1);
+        }
+        else
+        {
+            return A(m - 1, A(m, n - 1));
+        }
+    }
+
+    static void Main()
+    {
+        int m = 1;
+        int n = 3;
+        int result = A(m, n);
+        Console.WriteLine($"Аккерман({m}, {n}) = {result}");
+    }
+}
