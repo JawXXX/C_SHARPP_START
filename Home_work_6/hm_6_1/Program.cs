f56f19e8287aaa1db7762eca288fea7711874aae
slava <jaws_x@mail.ru>
2024-02-20 00:43:51 +0500

    Home_work_6_1

---
+++ Home_work_6/hm_6_1/Program.cs
@@ -0,0 +1,7 @@
+﻿char[,] charArray = new char[,] {
+    {'H', 'e', 'l', 'l', 'o'},
+    {'W', 'o', 'r', 'l', 'd'}
+};
+
+string result = string.Join(" ", charArray.Cast<char>());
+Console.WriteLine(result);
