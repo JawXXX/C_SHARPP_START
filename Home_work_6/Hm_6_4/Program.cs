e85b2403ba4fce90179a9052efb467f1342d1592
slava <jaws_x@mail.ru>
2024-02-20 02:14:42 +0500

    Home_wrk_6_3(2)

---
+++ Home_work_6/Hm_6_4/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+
+    static void Main(string[] args)
+    {
+        int[] myArray = { 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 };
+
+        Console.WriteLine;
+
+        for (int i = myArray.Length - 1; i >= 0; i--)
+        {
+            Console.WriteLine(myArray[i]);
+        }
+
+        Console.ReadLine();
+    }
+}
