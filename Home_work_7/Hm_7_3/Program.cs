274f4887d1a2b26bcd5f66a89b305218c0bbb972
slava <jaws_x@mail.ru>
2024-02-20 02:45:34 +0500

    Home_work_7_3

---
+++ Home_work_7/Hm_7_3/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] array = { 12, 89, 78, 56, 46 };
+        PrintArrayReversed(array, array.Length - 1);
+    }
+
+    static void PrintArrayReversed(int[] array, int index)
+    {
+        if (index < 0)
+            return;
+
+        Console.WriteLine(array[index]);
+        PrintArrayReversed(array, index - 1);
+    }
+}
