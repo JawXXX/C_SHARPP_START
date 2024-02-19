28474b44069b682c3207a0c7c73ff46e69d9af4c
slava <jaws_x@mail.ru>
2024-02-20 02:30:24 +0500

    Home_work_7_1

---
+++ Home_work_7/Hm_7_1/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void PrintNumbers(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.WriteLine(m);
+            PrintNumbers(m + 1, n);
+        }
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Введите значение M:");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение N:");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Натуральные числа от {m} до {n}:");
+        PrintNumbers(m, n);
+    }
+}
