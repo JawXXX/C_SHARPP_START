8520c20657b87b3deb1e8681fae3932ea25655de
slava <jaws_x@mail.ru>
2024-02-20 01:13:12 +0500

    Home_work_6_3

---
+++ Home_work_6/Hm_6_3/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string inputString = "оно";
+        
+        bool isPalindrome = IsPalindrome(inputString);
+
+        if (isPalindrome)
+        {
+            Console.WriteLine("Строка является палиндромом.");
+        }
+        else
+        {
+            Console.WriteLine("Строка не является палиндромом.");
+        }
+    }
+
+    static bool IsPalindrome(string str)
+    {
+        str = str.ToLower(); 
+        int i = 0;
+        int j = str.Length - 1;
+
+        while (i < j)
+        {
+            if (str[i] != str[j])
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
